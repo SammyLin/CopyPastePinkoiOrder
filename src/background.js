117fc3aa01fce41f95b2f52a8ed8c83f9b4435ba
Sammy Lin <sammylintw@gmail.com>
2016-09-15 17:36:56 +0800

    解決因為 pinkoi 後台開始支援 https 了，導致 判斷 url 的時候遇到 https 時會失敗

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
-    if (tab.url.indexOf("http://www.pinkoi.com/panel/order") != -1) { // Inspect whether the place where user clicked matches with our list of URL
+    if (tab.url.indexOf("www.pinkoi.com/panel/order") != -1) { // Inspect whether the place where user clicked matches with our list of URL
         chrome.tabs.executeScript(tab.id, {"file": "content.js"}, function () { 
         	alert('Pinkoi 的訂單資料，已經放入剪貼簿了');
         });
